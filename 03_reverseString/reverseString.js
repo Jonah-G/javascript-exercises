eeda8947af782a70710a0c0d90dd768af558cbe7
jonah.green <jonah.green@tills-MacBook-Pro.local>
2022-09-07 09:45:15 +1000

    finished fundamentals part 4 exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let stringArray = string.split('');
+    stringArray = stringArray.reverse();
+    const revString = stringArray.join('')
+    return revString;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
 
+    if (int1 < 0 || int2 < 0 || typeof int1 != 'number' || typeof int2 != 'number') {
+        return 'ERROR'
+    }
+
+    let temp = int1;
+    int1 = Math.min(int1, int2);
+    int2 = Math.max(temp, int2);
+
+    let sum = int1;
+
+    for (i = int1 +1; i <= int2; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
